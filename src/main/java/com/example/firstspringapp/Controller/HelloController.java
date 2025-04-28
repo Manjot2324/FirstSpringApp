aa90f7908aa1b5af1cdc4f7724ea9bf61279bc85
Manjot Singh <Manjot Singh>
2025-04-28 10:02:52 +0530

    Manjot [ADD] UC3.

--- src/main/java/com/example/firstspringapp/Controller/HelloController.java
+++ src/main/java/com/example/firstspringapp/Controller/HelloController.java
@@ -3,9 +3,11 @@ package com.example.firstspringapp.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
     @GetMapping("/")
     public String hello(){
@@ -16,4 +18,9 @@ public class HelloController {
         model.addAttribute("message", "Hello from BridgeLabz");
         return "hello lab"; // name of the HTML file (hello.html)
     }
+    @GetMapping("/Hello")
+    public String sayHello(){
+        return "Hello from Bridze-Labz";
+    }
+
 }
